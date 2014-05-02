27d4f46a07f6fd69381c06bf9c4f8c97220dd5d0
Adrian Price-Whelan <adrian.prw@gmail.com>
2014-05-02 10:23:48 -0400

    switching to class

--- scripts/lm10.py
+++ scripts/lm10.py
@@ -18,22 +18,30 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import astropy.units as u
+from astropy import log as logger
+logger.setLevel(logging.INFO)
 
 from streamteam.util import get_pool
 from streamteam.integrate import DOPRI853Integrator
 from streamteam.dynamics import lyapunov
 from streams.potential.lm10 import LawMajewski2010
 
-# Create logger
-logger = logging.getLogger(__name__)
+try:
+    x,v = os.path.split(__file__)
+    if x != "scripts":
+        raise IOError()
+except:
+    logger.error("You must run scripts from the top level in the cloned "
+                 "nonlinear-dynamics directory.")
+    sys.exit(1)
 
-plot_path = "plots/lm10"
-if not os.path.exists(plot_path):
-    os.makedirs(plot_path)
+# plot_path = "plots/lm10"
+# if not os.path.exists(plot_path):
+#     os.makedirs(plot_path)
 
-cache_path = "cache/lm10"
-if not os.path.exists(cache_path):
-    os.makedirs(cache_path)
+# cache_path = "cache/lm10"
+# if not os.path.exists(cache_path):
+#     os.makedirs(cache_path)
 
 # standard LM10 values:
 potential = LawMajewski2010()
@@ -49,107 +57,97 @@ def F(t, X, potential):
     dH_dq = potential._acceleration_at(X, nparticles, acc)
     return np.hstack((np.array([px, py, pz]).T, dH_dq))
 
-def ic_grid(dphi=10*u.deg, drdot=10*u.km/u.s):
-    # spacing between IC's in Phi and Rdot
-    dphi = dphi.decompose(potential.units).value
-    drdot = drdot.decompose(potential.units).value
-    max_rdot = (400*u.km/u.s).decompose(potential.units).value
-    max_phi = (360*u.deg).decompose(potential.units).value
-
-    # find the energy of Sgr orbit
-    pot = np.zeros((1,))
-    T = 0.5*np.sum(sgr_w[3:]**2)
-    V = lm10_potential(sgr_w[:3].reshape((1,3)), 1, pot, *potential_params)[0]
-    E = T + V
-
-    r = 20. # kpc
-    phidot = 0. # rad/Myr
-    theta = (90*u.deg).decompose(potential.units).value # rad
-
-    # T = 0.5*(rdot**2 + thetadot**2/r**2 + phidot**2/(r*np.sin(theta))**2)
-
-    w0s = []
-    for rdot in np.arange(0, max_rdot, drdot):
-        for phi in np.arange(0, max_phi, dphi):
-            X = r*np.cos(phi)*np.sin(theta)
-            Y = r*np.sin(phi)*np.sin(theta)
-            Z = r*np.cos(theta)
-            V = lm10_potential(np.vstack((X,Y,Z)).T, 1, pot, *potential_params)[0]
-
-            # solve for theta dot from energy
-            ptheta = r*np.sqrt(2*(E - V) - rdot**2 - phidot**2/(r*np.sin(theta))**2)
-            thetadot = ptheta / r**2
-
-            vx = rdot*np.sin(theta)*np.cos(phi) + r*np.cos(theta)*np.cos(phi)*thetadot - r*np.sin(theta)*np.sin(phi)*phidot
-            vy = rdot*np.sin(theta)*np.sin(phi) + r*np.cos(theta)*np.sin(phi)*thetadot + r*np.sin(theta)*np.cos(phi)*phidot
-            vz = rdot*np.cos(theta) - r*np.sin(theta)*thetadot
-
-            w0 = [X,Y,Z,vx,vy,vz]
-            if np.any(np.isnan(w0)): continue
-            w0s.append(w0)
-
-    w0s = np.array(w0s)
-    return w0s
-
-# TODO: support setting other parameters to fixed value
-def potential_grid(**kwargs):
-    if len(kwargs) > 2:
-        raise ValueError("Max of 2D grid.")
+def potential_grid(x, y, **kwargs):
+    if len(x) > 1 or len(y) > 1:
+        raise ValueError("Only 1D TODO")
 
     grids = []
-    for k,v in kwargs.items():
-        _min,_max,n = v
-        if hasattr(_min, "unit"):
-            _min = _min.decompose(potential.units).value
-            _max = _max.decompose(potential.units).value
+    name1,v1 = x.items()[0]
+    name2,v2 = y.items()[0]
 
-        grids.append((k,np.linspace(_min,_max,n)))
+    _min,_max,n = v1
+    if hasattr(_min, "unit"):
+        _min = _min.decompose(potential.units).value
+        _max = _max.decompose(potential.units).value
+    vals1 = np.linspace(_min,_max,n)
+
+    _min,_max,n = v2
+    if hasattr(_min, "unit"):
+        _min = _min.decompose(potential.units).value
+        _max = _max.decompose(potential.units).value
+    vals2 = np.linspace(_min,_max,n)
 
     param_dict_list = []
-    ix = 0
-    for elem1 in grids[0][1]:
-        for elem2 in grids[1][1]:
+    for elem1 in vals1:
+        for elem2 in vals2:
             g = dict()
-            g[grids[0][0]] = elem1
-            g[grids[1][0]] = elem2
-            g['index'] = ix
-            param_dict_list.append(g)
-            ix += 1
+            g[name1] = elem1
+            g[name2] = elem2
+            param_dict_list.append(dict(g.items()+kwargs.items()))
 
     return param_dict_list
 
-def lyapunov_map(w0, potential_params, **lyapunov_kwargs):
+def lyapunov_map_potential(w0, path, index_potential_params, **lyapunov_kwargs):
+    index,potential_params = index_potential_params
+
     potential = LawMajewski2010(**potential_params)
     integrator = DOPRI853Integrator(F, func_args=(potential,))
-    LE,w = lyapunov(w0, integrator, **lyapunov_kwargs)
+    LE,t,w = lyapunov(w0, integrator, **lyapunov_kwargs)
 
     # TODO: need a solution for this...
-    # np.save(os.path.join(path, "le_{}.npy".format(index)), LE)
-    # np.save(os.path.join(path, "orbit_{}.npy".format(index)), w)
+    np.save(os.path.join(path, "le_{}.npy".format(index)), LE)
+    np.save(os.path.join(path, "orbit_{}.npy".format(index)), w)
 
-    LE = np.mean(LE, axis=1)
+    # LE = np.mean(LE, axis=1)
 
-    # take only the 2nd half
-    y = LE[len(LE)//2:]
-    x = np.arange(0,len(y))
-    A = np.vstack([x, np.ones(len(x))]).T
-    m,b = np.linalg.lstsq(A, y)[0]
-    return [m,b]
+    # return LE
 
-def main(pool, grid_kwargs, **lyapunov_kwargs):
+def main(pool, grid1, grid2, **lyapunov_kwargs):
+
     # grid of potential parameter dicts
-    grid = potential_grid(**grid_kwargs)
+    grid = potential_grid(grid1, grid2)
 
-    # q1=(0.7,2.0,3),
-    # phi=(45*u.deg,135*u.deg,3))
+    func = partial(lyapunov_map_potential, sgr_w, path, **lyapunov_kwargs)
+    LEs = pool.map(func, enumerate(grid))
 
-    func = partial(lyapunov_map, sgr_w, **lyapunov_kwargs)
-    r = pool.map(func, grid)
+    return
+
+    r = []
+    p1,p2,p3 = False,False,False
+    for ii,LE in enumerate(LEs):
+        # take only the 2nd half
+        y = LE[len(LE)//2:]
+        x = np.arange(0,len(y))
+        A = np.vstack([x, np.ones(len(x))]).T
+        m,b = np.linalg.lstsq(A, y)[0]
+        r.append([m,b])
+
+        if m < -1E-7 and b > 0.0005 and not p1:
+            plot_that(LE, grid[ii][grid1.keys()[0]],
+                      grid[ii][grid2.keys()[0]], ii)
+            print("p1: {}".format(ii))
+            p1 = True
 
     k1,k2 = grid_kwargs.keys()
     names = [k1,k2,"m","b"]
     return np.array([(g[k1],g[k2],row[0],row[1]) for g,row in zip(grid,r)],
                     dtype=[(n,float) for n in names])
+
+def plot_that(LE, q1, qz, ii):
+    fig = plt.figure(figsize=(10,10))
+    ax = fig.add_subplot(111)
+    # fig2d = plt.figure(figsize=(10,10))
+    # ax2d = fig2d.add_subplot(111)
+
+    ax.cla()
+    ax.set_title("q1={}, qz={}".format(q1,qz))
+    ax.semilogy(LE, marker=None)
+    fig.savefig(os.path.join(plot_path,'le_{}.png'.format(ii)))
+
+    # ax2d.cla()
+    # ax2d.set_title("q1={}, qz={}".format(q1,qz))
+    # ax2d.plot(ws[:,0], ws[:,2], marker=None)
+    # fig2d.savefig(os.path.join(plot_path,'2d_orbit_{}.png'.format(ii)))
 
 if __name__ == "__main__":
     def _parse_grid(tup):
@@ -171,12 +169,12 @@ if __name__ == "__main__":
                         default=False, help="Overwrite cached files.")
 
     # potential parameter grid
-    parser.add_argument("--grid1", nargs=4, dest="grid1", required=True,
-                        help="Grid axis 1. Should be <param name> <min. val> "
-                        "<max. val> <num. vals>")
-    parser.add_argument("--grid2", nargs=4, dest="grid2", required=True,
-                        help="Grid axis 2. Should be <param name> <min. val> "
-                        "<max. val> <num. vals>")
+    # parser.add_argument("--grid1", nargs=4, dest="grid1", required=True,
+    #                     help="Grid axis 1. Should be <param name> <min. val> "
+    #                     "<max. val> <num. vals>")
+    # parser.add_argument("--grid2", nargs=4, dest="grid2", required=True,
+    #                     help="Grid axis 2. Should be <param name> <min. val> "
+    #                     "<max. val> <num. vals>")
 
     # threading
     parser.add_argument("--mpi", dest="mpi", default=False, action="store_true",
@@ -191,8 +189,8 @@ if __name__ == "__main__":
         logger.setLevel(logging.DEBUG)
     elif args.quiet:
         logger.setLevel(logging.ERROR)
-    else:
-        logger.setLevel(logging.INFO)
+
+    sys.exit(0)
 
     g1 = _parse_grid(args.grid1)
     g2 = _parse_grid(args.grid2)
@@ -207,7 +205,7 @@ if __name__ == "__main__":
 
     if not os.path.exists(cache_name):
         pool = get_pool(mpi=args.mpi, threads=args.threads)
-        lya_exps = main(pool, g, dt=10., nsteps=10000, noffset=4)
+        lya_exps = main(pool, g1, g2, dt=10., nsteps=10000, noffset=4)
         pool.close()
 
         header = ",".join(lya_exps.dtype.names)
@@ -321,3 +319,45 @@ if __name__ == "__main__":
     # plt.figure(figsize=(10,10))
     # plt.plot(w[:,0,0], w[:,0,2], marker=None, linestyle='-')
     # plt.savefig(os.path.join(plot_path, "sgr.png"))
+
+def ic_grid(dphi=10*u.deg, drdot=10*u.km/u.s):
+    # spacing between IC's in Phi and Rdot
+    dphi = dphi.decompose(potential.units).value
+    drdot = drdot.decompose(potential.units).value
+    max_rdot = (400*u.km/u.s).decompose(potential.units).value
+    max_phi = (360*u.deg).decompose(potential.units).value
+
+    # find the energy of Sgr orbit
+    pot = np.zeros((1,))
+    T = 0.5*np.sum(sgr_w[3:]**2)
+    V = lm10_potential(sgr_w[:3].reshape((1,3)), 1, pot, *potential_params)[0]
+    E = T + V
+
+    r = 20. # kpc
+    phidot = 0. # rad/Myr
+    theta = (90*u.deg).decompose(potential.units).value # rad
+
+    # T = 0.5*(rdot**2 + thetadot**2/r**2 + phidot**2/(r*np.sin(theta))**2)
+
+    w0s = []
+    for rdot in np.arange(0, max_rdot, drdot):
+        for phi in np.arange(0, max_phi, dphi):
+            X = r*np.cos(phi)*np.sin(theta)
+            Y = r*np.sin(phi)*np.sin(theta)
+            Z = r*np.cos(theta)
+            V = lm10_potential(np.vstack((X,Y,Z)).T, 1, pot, *potential_params)[0]
+
+            # solve for theta dot from energy
+            ptheta = r*np.sqrt(2*(E - V) - rdot**2 - phidot**2/(r*np.sin(theta))**2)
+            thetadot = ptheta / r**2
+
+            vx = rdot*np.sin(theta)*np.cos(phi) + r*np.cos(theta)*np.cos(phi)*thetadot - r*np.sin(theta)*np.sin(phi)*phidot
+            vy = rdot*np.sin(theta)*np.sin(phi) + r*np.cos(theta)*np.sin(phi)*thetadot + r*np.sin(theta)*np.cos(phi)*phidot
+            vz = rdot*np.cos(theta) - r*np.sin(theta)*thetadot
+
+            w0 = [X,Y,Z,vx,vy,vz]
+            if np.any(np.isnan(w0)): continue
+            w0s.append(w0)
+
+    w0s = np.array(w0s)
+    return w0s
